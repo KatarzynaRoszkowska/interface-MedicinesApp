b036cb7f3826137936d773930901fadbae71c4e8
M.Ziolkowski <ziolek0321@gmail.com>
2018-05-31 14:44:50 +0200

    Dziala poprawnie pobieranie i wyswietlanie lekow

--- app/src/main/java/pl/roszkowska/med/MyMedicinesApplication.java
+++ app/src/main/java/pl/roszkowska/med/MyMedicinesApplication.java
@@ -44,7 +44,7 @@ public class MyMedicinesApplication extends Application {
         super.onCreate();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.2.11.33:8080") // Adres serwera
+                .baseUrl("http://172.20.10.3:8080") // Adres serwera
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
